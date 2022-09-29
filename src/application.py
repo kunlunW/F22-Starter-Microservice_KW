4bd8da834d0c9f13b5d942e8aa5576ac3d12050e
kunlunW <kwang358@wisc.edu>
2022-09-28 22:38:09 -0400

    9/28 edit

--- src/application.py
+++ src/application.py
@@ -12,6 +12,13 @@ app = Flask(__name__,
 
 CORS(app)
 
+# if a network request comes in and begins with this, then execute the follow def get_health() method
+# There's a program that's listening for messages. The @app line is called a route (the route to the function)
+# If we execute this python file and it will produce two debugging links, and if we click into one of the links
+# it will say "Not Found", this is because we didn't define that route, there's no function there
+# But if we add api/health after that, it will work.
+# The purpose for this assignment is that we'll take this and upload this to the amazon cloud
+
 
 @app.get("/api/health")
 def get_health():
@@ -21,11 +28,26 @@ def get_health():
         "health": "Good",
         "at time": t
     }
+    import os
+    print(os.environ)
 
     # DFF TODO Explain status codes, content type, ... ...
+    # I'm returning to some program app that called this function
+    # There are three parts to the response object
+    # first parameter: data
+    # second parameter: status code
+    # third parameter: content type, which defines if the data is a string, image, audio file, etc.
+
+    # There are some tools that can be quite useful: postman
+    # Browsers send GET request, there are other operations e.g. POST, PUT, DELETE
     result = Response(json.dumps(msg), status=200, content_type="application/json")
 
     return result
+
+# <uni> is called a path parameter, the function will get student by uni
+# This a route that define that network request that comes in the form /api/students/###
+# it will execute the following function
+# This is also called binding, you have bound a method in python to a specific network point
 
 
 @app.route("/api/students/<uni>", methods=["GET"])
--- src/columbia_student_resource.py
+++ src/columbia_student_resource.py
@@ -8,12 +8,14 @@ class ColumbiaStudentResource:
     def __int__(self):
         pass
 
+    # This is a static function that will connect to the database
+
     @staticmethod
     def _get_connection():
 
-        usr = os.environ.get("DBUSER")
-        pw = os.environ.get("DBPW")
-        h = os.environ.get("DBHOST")
+        usr = os.environ.get("DB_Username")
+        pw = os.environ.get("DB_Password")
+        h = os.environ.get("DB_Host")
 
         conn = pymysql.connect(
             user=usr,
@@ -29,6 +31,7 @@ class ColumbiaStudentResource:
 
         sql = "SELECT * FROM f22_databases.columbia_students where guid=%s";
         conn = ColumbiaStudentResource._get_connection()
+        print("connecting successful!")
         cur = conn.cursor()
         res = cur.execute(sql, args=key)
         result = cur.fetchone()
